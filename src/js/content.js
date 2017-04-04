4867b39016c84a69ba27d8e20b4b12d70b395d79
Thomas Castiglione <castiglione@mac.com>
2017-04-05 05:49:23 +0800

    attach to extra editors after the initial load

--- src/js/content.js
+++ src/js/content.js
@@ -1,29 +1,36 @@
 for (var key in window.CKEDITOR.instances) {
     if (CKEDITOR.instances.hasOwnProperty(key)) {
-        var instance = CKEDITOR.instances[key];
-        var container = instance.element.$.parentElement;
-
-        var plainEditor = document.createElement("textarea");
-        plainEditor.style.height = "200px";
-
-        var reentrant = false;
-        plainEditor.onkeyup = function() {
-            if (!reentrant)
-            {
-                reentrant = true;
-                instance.setData(plainEditor.value);
-                reentrant = false;
-            }
-        }
-        instance.on("change", function() {
-            if (!reentrant)
-            {
-                reentrant = true;
-                plainEditor.value = instance.getData();
-                reentrant = false;
-            }
-        });
-
-        container.appendChild(plainEditor);
+        attach(CKEDITOR.instances[key]);
     }  
+}
+
+CKEDITOR.on("instanceReady", function(e) {
+    attach(e.editor);
+});
+
+function attach(instance) {
+    var container = instance.element.$.parentElement;
+
+    var plainEditor = document.createElement("textarea");
+    plainEditor.style.height = "200px";
+
+    var reentrant = false;
+    plainEditor.onkeyup = function() {
+        if (!reentrant)
+        {
+            reentrant = true;
+            instance.setData(plainEditor.value);
+            reentrant = false;
+        }
+    }
+    instance.on("change", function() {
+        if (!reentrant)
+        {
+            reentrant = true;
+            plainEditor.value = instance.getData();
+            reentrant = false;
+        }
+    });
+
+    container.appendChild(plainEditor);
 }
